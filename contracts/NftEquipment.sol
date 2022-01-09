cf76c8b4bb42751e0cf97bab03ffeaada670afd9
Carlos Calderon <cdcalderon@gmail.com>
2022-01-08 21:34:02 -0600

    add main structure NFTEquipment

--- contracts/NftEquipment.sol
+++ contracts/NftEquipment.sol
@@ -2,3 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+contract NFTFarm is Ownable, ERC1155Holder {
+    struct UserInfo {
+        uint256 stakedAmount;
+        uint256 lastUpdateAt;
+        uint256 pointsDebt;
+    }
+
+    struct NFTInfo {
+        uint256 id;
+        uint256 remaining;
+        uint256 price;
+    }
+
+    constructor(uint256 _emissionRate, DaiToken _token) {
+        emissionRate = _emissionRate;
+        token = _token;
+    }
+}
