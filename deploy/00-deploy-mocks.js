645c9f0ae0ec473ecd07db9e18f81def5141a5ee
Carlos Calderon <cdcalderon@gmail.com>
2022-01-23 22:10:17 -0600

    add condition to deployment contract

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -4,5 +4,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    // If we are on a local development network, we need to deploy mocks!
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+    }
 }
 module.exports.tags = ["all", "mocks"]
